9d1ee61c1218adbdc269a48074e889f608a3eedb
Kevin Chen <kevin@uken.com>
2015-01-09 10:53:15 -0500

    Complete config modifications for integration of AngularJS into Rails. Set up basic home index.

--- Tetris/config/application.rb
+++ Tetris/config/application.rb
@@ -2,13 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-#Bower for AngularJS
-config.assets.paths << Rails.root.join("vendor","assets","bower_components")
-config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
-
-config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
-
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -26,6 +19,12 @@ module RailsHighscore
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    #Bower for AngularJS
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components")
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
+
+    config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
